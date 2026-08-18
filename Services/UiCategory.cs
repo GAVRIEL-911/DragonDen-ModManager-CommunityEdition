d4e0919e4723ad40fbb97ce906c5055bfc2d3a04
GAVRIEL-911 <srikanthknipper622@gmail.com>
2026-08-18 20:37:03 +0800

    Delete Services directory

--- Services/UiCategory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DragonDen.ModManager.Services;
-
-public sealed class UiCategory
-{
-    public string Title { get; set; } = "";
-    public string Slug { get; set; } = "";
-    public string ColorClass { get; set; } = "";
-}
